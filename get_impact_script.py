9c69bf4d5b43aabfce41017456e3a31be7e3495e
ShreyanSen <33622797+ShreyanSen@users.noreply.github.com>
2020-05-03 16:42:11 -0700

    Update get_impact_script.py

--- get_impact_script.py
+++ get_impact_script.py
@@ -28,10 +28,12 @@ def main_script(history_csv_name, titles_csv_name, output_name):
     impact.to_csv(output_name)
     return 
 
+
 def multiply_cols_by_ref_col(df, ref_col, col_list):
     for colname in col_list:
         df.loc[:, colname] = df[colname]*df[ref_col]
     return df
+
 
 def read_data(history_csv_name, titles_csv_name):
     history = pd.read_csv(history_csv_name, header=1)
@@ -39,15 +41,18 @@ def read_data(history_csv_name, titles_csv_name):
     titles = titles[['Active_Product','Include_Quant','Line item title','Correct_Title','Girls ','Wildlife','Trees']]
     return history, titles
 
+
 def clean_and_merge_data(history, titles):
     history_2 = history[['Customer email','Customer first name','Customer last name','Line item title','Total quantity']].merge(titles, on=['Line item title'], how='left')
     history_2 = history_2.loc[history_2.Include_Quant==1, :]
     return history_2
 
+
 def get_impact(history_2):
     history_3 = multiply_cols_by_ref_col(history_2, 'Total quantity', ['Girls ','Trees','Wildlife'])
     impact = history_3.groupby(['Customer email','Customer first name','Customer last name']).agg({'Girls ':'sum','Wildlife':'sum','Trees':'sum'}).reset_index()
     return impact
+
 
 if __name__=='__main__':
     history_csv_name, titles_csv_name, output_name = get_args()
